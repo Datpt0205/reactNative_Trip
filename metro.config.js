29aa382cb5a8425a86efc13dc53e4eb75ec39164
Datpt0205 <86192047+Datpt0205@users.noreply.github.com>
2022-10-26 18:25:25 +0700

    add validate

--- metro.config.js
+++ metro.config.js
@@ -5,3 +5,5 @@ const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.assetExts.push("cjs");
 
 module.exports = defaultConfig;
+
+
--- screens/Home.js
+++ screens/Home.js
@@ -18,11 +18,11 @@ const Home = () => {
   const tripRef = firebase.firestore().collection("trips");
   const [addHeading, setAddHeading] = useState("");
   const [addDestination, setAddDestination] = useState("");
-  const [addDate, setAddDate] = useState("");
+  const [addDate, setAddDate] = useState(new Date(1598051730000));
   const [addRisk, setAddRisk] = useState("");
   const [addDescription, setAddDescription] = useState("");
   const navigation = useNavigation();
-
+  
   //fetch data
   useEffect(() => {
     tripRef.orderBy("createdAt", "desc").onSnapshot((querySnapshot) => {
@@ -31,11 +31,11 @@ const Home = () => {
         const { heading, destination, date, risk, description } = doc.data();
         trips.push({
           id: doc.id,
-          heading, 
-          destination, 
-          date, 
-          risk, 
-          description
+          heading,
+          destination,
+          date,
+          risk,
+          description,
         });
       });
       setTrips(trips);
@@ -47,7 +47,8 @@ const Home = () => {
   const deleteTrip = (trips) => {
     tripRef
       .doc(trips.id)
-      .delete().then(() => {
+      .delete()
+      .then(() => {
         //show a successful alert
         alert("Deleted successfully");
       })
@@ -58,16 +59,24 @@ const Home = () => {
 
   //Add trip
   const addTrip = (trip) => {
-      //get the timestamp
-      const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-      const data = {
-        heading: addHeading, 
-        destination: addDestination, 
-        date:addDate, 
-        risk:addRisk, 
-        description:addDescription,
-        createdAt: timestamp,
-      };
+    //get the timestamp
+    const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+    const data = {
+      heading: addHeading,
+      destination: addDestination,
+      date: addDate,
+      risk: addRisk,
+      description: addDescription,
+      createdAt: timestamp,
+    };
+    if (
+      addHeading.length == 0 ||
+      addDestination.length == 0 ||
+      addDate.length == 0 ||
+      addRisk.length == 0
+    ) {
+      alert("Need to fill all required fields!");
+    } else {
       tripRef
         .add(data)
         .then(() => {
@@ -83,14 +92,15 @@ const Home = () => {
           alert(err);
         });
     }
+  };
 
   return (
     <View style={{ flex: 1 }}>
-    <Text style = {styles.firstText}>ADD A NEW TRIP</Text>
+      <Text style={styles.firstText}>ADD A NEW TRIP</Text>
       <View style={styles.formContainer}>
         <TextInput
           style={styles.input}
-          placeholder="Name of trip"
+          placeholder="Name of trip*"
           placeholderTextColor="#aaaaaa"
           onChangeText={(heading) => setAddHeading(heading)}
           value={addHeading}
@@ -99,7 +109,7 @@ const Home = () => {
         />
         <TextInput
           style={styles.input}
-          placeholder="Destination"
+          placeholder="Destination*"
           placeholderTextColor="#aaaaaa"
           onChangeText={(destination) => setAddDestination(destination)}
           value={addDestination}
@@ -108,7 +118,7 @@ const Home = () => {
         />
         <TextInput
           style={styles.input}
-          placeholder="dd/MM/yyyy"
+          placeholder="dd/MM/yyyy*"
           placeholderTextColor="#aaaaaa"
           onChangeText={(date) => setAddDate(date)}
           value={addDate}
@@ -117,7 +127,7 @@ const Home = () => {
         />
         <TextInput
           style={styles.input}
-          placeholder="Risk Assessment: Yes or No"
+          placeholder="Risk Assessment: Yes or No*"
           placeholderTextColor="#aaaaaa"
           onChangeText={(risk) => setAddRisk(risk)}
           value={addRisk}
@@ -154,16 +164,19 @@ const Home = () => {
               />
               <View style={styles.innerContainer}>
                 <Text style={styles.itemHeading}>
-                    {item.heading[0].toUpperCase() + item.heading.slice(1)}
+                  {item.heading[0] + item.heading.slice(1)}
                 </Text>
                 <Text style={styles.itemDestination}>
-                    {item.destination[0].toUpperCase() + item.destination.slice(1)}
+                  {item.destination[0].toUpperCase() +
+                    item.destination.slice(1)}
                 </Text>
+              </View>
+              <View style={styles.innerContainer}>
                 <Text style={styles.itemDate}>
-                    {item.date[0].toUpperCase() + item.date.slice(1)}
+                  {"Date: " + item.date[0].toUpperCase() + item.date.slice(1)}
                 </Text>
-                <Text style={styles.itemDescription}>
-                    {item.description[0].toUpperCase() + item.description.slice(1)}
+                <Text style={styles.itemRisk}>
+                  {"Risk Assessment: " + item.risk[0].toUpperCase() + item.risk.slice(1)}
                 </Text>
               </View>
             </Pressable>
@@ -177,87 +190,88 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'grid',
-        backgroundColor:'#e5e5e5',
-        padding:15,
-        borderRadius: 15,
-        margin:5,
-        marginHorizontal: 10,
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    innerContainer:{
-        alignItems: 'center',
-        flexDirection: 'row',
-        marginLeft: 45,
-    },
-    itemHeading:{
-        fontWeight: 'bold',
-        fontSize: 10,
-        marginRight: 22,
-
-    },
-    itemDestination:{
-        fontWeight: 'bold',
-        fontSize: 10,
-        marginRight: 22,
-    },
-    itemDate:{
-        fontWeight: 'bold',
-        fontSize: 10,
-        marginRight: 22,
-    },
-    itemRisk:{
-        fontWeight: 'bold',
-        fontSize: 10,
-        marginRight: 22,
-    },
-    itemDescription:{
-        fontWeight: 'bold',
-        fontSize: 10,
-        marginRight: 22,
-    },
-    formContainer: {
-        // flexDirection: 'row',
-        height: 300,
-        marginLeft:10,
-        marginRight:10,
-        marginTop:50,
-    },
-    input:{
-        height: 40,
-        borderRadius: 10,
-        overflow: 'hidden',
-        backgroundColor: 'white',
-        paddingLeft:16,
-        flex:1,
-        marginRight: 5,
-        marginTop: 10,
-    },
-    button:{
-        height: 47,
-        borderRadius: 15,
-        backgroundColor: '#788eec',
-        width:80,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginLeft: 310,
-        marginTop: 10,
-    },
-    buttonText:{
-        color: 'white',
-        fontSize:20,
-    },
-    tripIcon:{
-        marginTop: 5,
-        fontSize:20,
-        marginLeft:14,
-    }, 
-    firstText: {
-        marginTop:10,
-        marginLeft: 120,
-        fontWeight: 'bold',
-        fontSize: 20,
-    }
-})
+  container: {
+    display: "grid",
+    backgroundColor: "#e5e5e5",
+    padding: 15,
+    borderRadius: 15,
+    margin: 5,
+    marginHorizontal: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  innerContainer: {
+    alignItems: "center",
+    flexDirection: "column",
+    marginLeft: 45,
+  },
+  itemHeading: {
+    fontWeight: "bold",
+    fontSize: 10,
+    marginRight: 22,
+  },
+  itemDestination: {
+    fontWeight: "bold",
+    fontSize: 10,
+    marginRight: 22,
+    paddingTop: 10,
+  },
+  itemDate: {
+    fontWeight: "bold",
+    fontSize: 10,
+    marginRight: 22,
+  },
+  itemRisk: {
+    fontWeight: "bold",
+    fontSize: 10,
+    marginRight: 22,
+    paddingTop: 10,
+  },
+  itemDescription: {
+    fontWeight: "bold",
+    fontSize: 10,
+    marginRight: 22,
+  },
+  formContainer: {
+    // flexDirection: 'row',
+    height: 300,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 50,
+  },
+  input: {
+    height: 40,
+    borderRadius: 10,
+    overflow: "hidden",
+    backgroundColor: "white",
+    paddingLeft: 16,
+    flex: 1,
+    marginRight: 5,
+    marginTop: 10,
+  },
+  button: {
+    height: 47,
+    borderRadius: 15,
+    backgroundColor: "#788eec",
+    width: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 310,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+  },
+  tripIcon: {
+    marginTop: 5,
+    fontSize: 20,
+    marginLeft: 14,
+  },
+  firstText: {
+    marginTop: 10,
+    marginLeft: 120,
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
